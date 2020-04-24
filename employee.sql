f3c41d30eeef0067402e6a939e0fa6fdad49e1ef
naritchaya louis <naritchayalouis@gmail.com>
2020-04-24 15:39:52 -0500

    created tables

---
+++ employee.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(30) NULL,
+PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NULL,
+salary DECIMAL,
+department_id INT,
+PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NULL,
+last_name VARCHAR(30) NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
+
+);
+
