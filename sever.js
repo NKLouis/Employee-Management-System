0d3fb3c6a29b72ee9f37ba28d4bdde7dd413c493
naritchaya louis <naritchayalouis@gmail.com>
2020-04-25 22:32:41 -0500

    added .gitignore file

--- sever.js
+++ sever.js
@@ -1,71 +1,261 @@
-// var mysql = require("mysql");
+    var mysql = require("mysql");
+    var inquirer = require("inquirer");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
+    var connection = mysql.createConnection({
+        host: "localhost",
 
-//   // Your port; if not 3306
-//   port: 3306,
+        // port; if not 3306
+        port: 3306,
 
-//   // Your username
-//   user: "root",
+        // Your username
+        user: "root",
 
-//   // Your password
-//   password: "6963",
-//   database: "employee_DB"
-// });
+        // Your password
+        password: "6963",
+        database: "employee_DB"
+    });
 
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log("connected as id " + connection.threadId + "\n");
-//   inits();
-// });
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId + "\n");
+        start();
+    });
 
-// function inits(){
-//     inquirer
-//     .prompt([
-//         {
-//         type: "list",
-//         message: "What would you like to do?",
-//         choices: ["Add", "Update","View"],
-//         name: "question"
-//         }
+// function which prompts the user for what action they should take
+    function start() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "What would you like to do?",
+                    choices: ["Add", "View", "Update", "Delete"],
+                    name: "options"
+                }
 
-//     ]).then(response => {
-//         switch (response.question){
+            ]).then(response => {
+                switch (response.options) {
 
-//         case "Add": add()
+                    case "Add": addInfo();
+                    break;
 
-//     }
+                    case "View": viewInfo();
+                    break;
 
-//     });
-// };
-//     function add(){
-//         inquirer
-//         .prompt([
-//             {
-//             type: "list",
-//             message: "What would you like to add?",
-//             choices: ["departments", "roles","employees"],
-//             name: "add"
-//             }
-    
-//         ]).then(response => {
-//             switch(response.add){
+                    case "Update": updateInfo();
+                    break;
 
-//                case "departments" : addDepartment()
+                    case "Delete": deleteInfo();
+                    break;
 
-//                break;
-//                case "roles": addRoles()
-//                break;
+                }
+            });
+    };
 
-//                case "employees" : addEmployees()
-//                break;
+    function addInfo() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "What would you like to add?",
+                    choices: ["departments", "roles", "employees"],
+                    name: "add"
+                }
 
-//                default: console.log("err in the add function")
+            ]).then(response => {
+                switch (response.add) {
 
+                    case "departments": addDepartment();
 
-//             }
+                        break;
 
+                    case "roles": addRoles();
+
+                        break;
+
+                    case "employees": addEmployees();
+
+                        break;
+
+                    default: console.log("err in the add function")
+                };
+            });
+    };
+    function addDepartment() {
+        inquirer
+        .prompt([
+          {
+            
+            type: "input",
+            message: "What department would you like to add?",
+            name: "item",
+          },
+        ])
+        .then(function(addDepartment) {
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                  name: addDepartment.item,
+                },
+                function(err) {
+                    if (err) throw err;
+                    console.log("Your department was created successfully!");
+                })      
+        })
+    };
         
-//     });
-// };
+    
+
+//***************************************************************
+    function viewInfo() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "What would you like to view?",
+                    choices: ["departments", "roles", "employees"],
+                    name: "view"
+                }
+
+            ]).then(response => {
+                switch (response.view) {
+
+                    case "departments": viewDepartment();
+
+                        break;
+
+                    case "roles": viewRoles();
+
+                        break;
+
+                    case "employees": viewEmployees();
+
+                        break;
+
+                    default: console.log("err in the view function")
+                };
+            });
+    };
+
+    function viewDepartment(){
+        connection.query("SELECT * FROM department", function(err, results) {
+            if (err) throw err;
+        console.log(results);
+
+
+        });
+    };
+//*********************************************************** 
+    function updateInfo() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "What would you like to update?",
+                    choices: ["employeeRoles", "employeeManagers"],
+                    name: "update"
+                }
+
+            ]).then(response => {
+                switch (response.update) {
+
+                    case "employeeRoles": updateRoles();
+
+                        break;
+
+                    case "employeeManagers": updateManagers();
+
+                        break;
+
+                    default: console.log("err in the update function")
+                };
+            });
+    };
+    function updateRoles(){
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What role would you like to update?",
+                    name: "updateRoles"
+                },
+                {
+                    type: "input",
+                    message: "What role would you like to change it to?",
+                    name: "newRoles"
+                }
+            ])
+            .then(response => {
+                connection.query("UPDATE role SET ? WHERE ?",
+                [
+                    {
+                     title: response.newRoles
+                    },
+                    {
+                    title: response.updateRoles  
+                    }
+                ],
+                function(err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows + " products updated!\n");
+                }
+                )
+                
+
+            })
+
+
+
+
+    }
+
+
+
+    //*********************************************************** 
+    function deleteInfo() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "What would you like to delete?",
+                    choices: ["departments", "roles", "employees"],
+                    name: "delete"
+                }
+
+            ]).then(response => {
+                switch (response.delete) {
+
+                    case "roles": deleteRoles();
+
+                        break;
+
+                    case "managers": deleteManagers();
+
+                        break;
+
+                    default: console.log("err in the delete function")
+                };
+            });
+    };
+    function deleteRoles(){
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What role would you like to delete?",
+                    name: "deleteRoles"
+                },
+            ])
+            .then(response => {
+                connection.query(
+                    "DELETE FROM role WHERE ?",
+                    {
+                      title: response.deleteRoles
+                    },
+                    function(err, res) {
+                        if (err) throw err;
+                        console.log(res.affectedRows + " products deleted!\n");
+                    })
+            })
+        }
+
+
